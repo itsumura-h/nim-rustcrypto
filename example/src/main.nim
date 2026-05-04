47ad0dc5c1c0abab568178670fb065d646c3d6c6
itsumura-h <dumblepy@gmail.com>
2026-05-04 18:18:42 +0000

    Refactor secp256k1 functions to use updated API methods
    - Replaced direct calls to secp256k1 functions with new high-level API methods for generating secret keys, public keys, signing, and verification.
    - Improved code clarity and maintainability by utilizing the updated RustCrypto API structure.
    - Ensured consistency with recent changes in cryptographic module design.

--- example/src/main.nim
+++ example/src/main.nim
@@ -1,10 +1,9 @@
 import rustcrypto/algorithm/secp256k1
-import rustcrypto/algorithm/sha256
 
-let secretKey = randomSecretKey()
-let publicKey = secp256k1PublicKeyCompressed(secretKey)
-let signature = secp256k1EcdsaSignSha256("abc", secretKey)
-let verification = secp256k1EcdsaVerifySha256("abc", publicKey, signature)
+let secretKey = Secp256k1.generateSecretKey()
+let publicKey = Secp256k1.publicKeyCompressed(secretKey)
+let signature = Secp256k1.sign("abc", secretKey)
+let verification = Secp256k1.verify("abc", publicKey, signature)
 
 echo("secretKey: ", secretKey)
 echo("publicKey: ", publicKey)
