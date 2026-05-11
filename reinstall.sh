d652590635e1a677266848fcdd203c933766637c
itsumura-h <dumblepy@gmail.com>
2026-05-11 07:05:29 +0000

    Update RustCrypto FFI build scripts and version
    - Added support for the `wasm32-wasip1` target in both `reinstall.sh` and `test.sh` scripts to enhance WebAssembly compatibility.
    - Updated the RustCrypto FFI version to `0.1.3` in `Cargo.lock` to reflect recent changes and improvements.
    - Streamlined the build process for Rust static libraries, ensuring all targets are correctly built and tested.

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 cd /application/src/rustcrypto-ffi
+rustup target add wasm32-unknown-unknown wasm32-wasip1
 cargo build --release --lib
+cargo build --release --lib --target wasm32-unknown-unknown
+cargo build --release --lib --target wasm32-wasip1
 
 cd /application/src/nim-rustcrypto
 nim r --hints:off --warnings:off src/rustcrypto/tools/sync_local_rustcrypto_ffi.nim
